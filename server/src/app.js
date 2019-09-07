2f01497bc089bbb1c684373350155de4d9458e0a
Ryland Wang <rylandwang@gmail.com>
2019-09-07 17:21:05 -0400

    create socket save drawing method

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,8 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 const documents = {};
+drawings = []
+const PORT = 4444;
 
 /** Event listener */
 io.on('connection', socket => {
@@ -26,8 +28,23 @@ io.on('connection', socket => {
     });
 
     socket.on('editDoc', doc => {
+        console.log("edit")
         documents[doc.id] = doc;
         socket.to(doc.id).emit('document', doc);
+    });
+
+    socket.on('data', function (data) {
+        console.log(socket.id +': ' + data.msg);
+        var message = {from: socket.id,
+                       msg: data.msg
+                       }
+        io.emit('newMsg', message)
+    })
+
+    socket.on('submitDrawing', generatedString => {
+        drawings.push(generatedString)
+        console.log(drawings)
+        io.emit('drawings', drawings)
     });
 
     io.emit('documents', Object.keys(documents));
@@ -35,6 +52,6 @@ io.on('connection', socket => {
     console.log(`Socket ${socket.id} has connected`);
 });
 
-http.listen(4200, () => {
-    console.log('Listening on port 4200');
+http.listen(PORT, () => {
+    console.log('Listening on port ', PORT);
 });
