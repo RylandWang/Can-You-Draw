74d3155ab31bd7f9090deb4a90935844ab0febd7
Ryland Wang <rylandwang@gmail.com>
2019-09-15 18:26:28 -0400

    implement in-game UI

--- client/src/app/components/document/document.component.ts
+++ client/src/app/components/document/document.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
-import { DocumentService } from 'src/app/services/document.service';
+import { Component, OnInit, OnDestroy, ViewChild, Input } from '@angular/core';
+import { SocketService } from 'src/app/services/socket.service';
 import { Subscription, Observable } from 'rxjs';
-import { Document } from 'src/app/models/document';
-import { startWith } from 'rxjs/operators';
-import { CanvasWhiteboardComponent, CanvasWhiteboardUpdate } from 'ng2-canvas-whiteboard';
-import { Socket } from 'ngx-socket-io';
+import { CanvasWhiteboardComponent, CanvasWhiteboardUpdate, CanvasWhiteboardService } from 'ng2-canvas-whiteboard';
+import { Player } from 'src/app/models/player';
+import { Time } from '@angular/common';
 
 @Component({
   selector: 'app-document',
@@ -12,40 +11,58 @@ import { Socket } from 'ngx-socket-io';
   styleUrls: ['./document.component.scss'],
   viewProviders: [CanvasWhiteboardComponent],
 })
-export class DocumentComponent implements OnInit, OnDestroy {
-  document: Document;
-  private _docSub: Subscription;
-  constructor(private documentService: DocumentService, private socket: Socket) { }
+export class DocumentComponent implements OnInit {
 
   @ViewChild('canvasWhiteboard') canvasWhiteboard: CanvasWhiteboardComponent;
   generatedString: string;
   outputs: string[]
   drawings: Observable<string[]>
 
-  ngOnInit() {
-    this._docSub = this.documentService.currentDocument.pipe(
-      startWith({ id: '1234', doc: 'Select an existing document or create a new one to get started'})
-    ).subscribe(document => this.document = document);
-    this.drawings = this.documentService.drawings;
-    this.drawings.subscribe(x=>console.log(x))
+  @Input() players: Observable<Player[]>
+  @Input() player: Player
+
+  timeLeft: number = 120;
+  interval;
+
+  drawingComplete: boolean = false;
+
+  constructor(private readonly socketService: SocketService,
+    private _canvasWhiteboardService: CanvasWhiteboardService) {
   }
 
-  ngOnDestroy() {
-    this._docSub.unsubscribe();
+  ngOnInit() {
+    this.startTimer()
+    // set canvas height to be 0.8*window.height
+    document.getElementById("canvas").style.height = (window.innerHeight * 0.8).toString() + "px";
+    this.socketService.playerLeave(this.player)
+    this.socketService.playerJoin(this.player)
+
+    this.drawings = this.socketService.drawings;
+
+
   }
 
   sendBatchUpdates(updates: CanvasWhiteboardUpdate[]) {
     console.log("batch update")
     this.generatedString = this.canvasWhiteboard.generateCanvasDataUrl("image/jpeg", 0.3);
-    this.document.doc = this.generatedString
+
     // this.documentService.sendData(this.generatedString)
     // this.documentService.editDocument(this.document)
   }
 
-  onClear(){
-    console.log("clear")
-    this.documentService.submitDrawing(this.generatedString)
+  submitDrawing() {
+    this.socketService.submitDrawing(this.generatedString)
+    this.drawingComplete = true
+  }
 
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.timeLeft = 60;
+      }
+    }, 1000)
   }
 }
-
